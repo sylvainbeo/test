e1747c0dc114876ec65f20fb04c3a4aeb9916e36
sylvain beorchia <sylvain.beorchia@oslandia.com>
2016-06-03 12:40:07 +0200

    New delta

---
+++ update/delta/delta_2.0.0_10052016.sql
@@ -0,0 +1,1 @@
+CREATE TABLE qwat_od.ccc (id serial);
--- update/verify_upgrade_db.sh
+++ update/verify_upgrade_db.sh
@@ -75,15 +75,16 @@ echo "Getting lastest Tag num"
 cd $DIR
 LATEST_TAG=$(git describe)
 #PROPER_LATEST_TAG=$(echo $LATEST_TAG| cut -d'-' -f 1)
-PROPER_LATEST_TAG=$(echo $LATEST_TAG| cut -c 2)
-printf "Latest tag = ${GREEN}$PROPER_LATEST_TAG${NC}\n"
+SHORT_LATEST_TAG=$(echo $LATEST_TAG| cut -c 1)
+printf "Latest tag = ${GREEN}$SHORT_LATEST_TAG${NC}\n"
 
 echo "Applying deltas on $TESTCONFORMDB:"
 for f in $DIR/delta/*.sql
 do
     CURRENT_DELTA=$(basename "$f")
-    CURRENT_DELTA_NUM_VERSION=$(echo $CURRENT_DELTA| cut -d'_' -f 2)
-    if [[ $CURRENT_DELTA_NUM_VERSION > $NUMVERSION || $PROPER_LATEST_TAG == '' ]]; then
+    #CURRENT_DELTA_NUM_VERSION=$(echo $CURRENT_DELTA| cut -d'_' -f 2)
+    CURRENT_DELTA_NUM_VERSION=$(echo $CURRENT_DELTA| cut -c 7)
+    if [[ $CURRENT_DELTA_NUM_VERSION > $NUMVERSION || $SHORT_LATEST_TAG == '' ]]; then
         printf "    Processing ${GREEN}$CURRENT_DELTA${NC}, num version = $CURRENT_DELTA_NUM_VERSION\n"
         /usr/bin/psql --host $HOST --port 5432 --username "$USER" --no-password -q -d "$TESTCONFORMDB" -f $f
     else
@@ -95,16 +96,35 @@ echo "Producing referential file for test_qwat DB (from $QWATSERVICETEST)"
 /usr/bin/psql --host $HOST --port 5432 --username "$USER" --no-password -d "$QWATSERVICETEST" -f test_migration.sql > test_migration.expected.sql
 
 
+# !!!!!!!!!    TODO We need to execute init_qwat.sh from the lastest TAG version in $QWATSERVICETESTCONFORM
+# Saving current branch
+CURRENT_BRANCH=git rev-parse --abbrev-ref HEAD
+
+PROPER_LATEST_TAG=$SHORT_LATEST_TAG".0.0"
+echo "Switching on lastest tag major version ($PROPER_LATEST_TAG)"
+git checkout tags/$PROPER_LATEST_TAG
+
+echo "Initializing qwat DB in qwat_test_conform"
+./init_qwat.sh -p $QWATSERVICETESTCONFORM -d > init_qwat.log
+
+echo "Switching back to current branch ($PROPER_LATEST_TAG)"
+git checkout $CURRENT_BRANCH
+
+
+#  By the way, this cannot work if there is no TAG version
+
+
 echo "Performing conformity test"
 STATUS=$(python test_migration.py --pg_service $QWATSERVICETESTCONFORM)
 
 if [[ $STATUS == "DataModel is OK" ]]; then
     printf "${GREEN}Migration TEST is successfull${NC}. You may now migrate your real DB\n"
+    EXITCODE=0
 else
     printf "${RED}Migration TEST has failed${NC}. Please contact qWat team and give them the following output :\n $STATUS \n\n"
+    EXITCODE=1
 fi
 
-EXITCODE=0
 exit $EXITCODE
 
 
@@ -115,6 +135,3 @@ exit $EXITCODE
 # 
 # # TODO : dropping qwat_test
 # # TODO : dropping qwat_test_conform
-# 
-# EXITCODE=0
-# exit $EXITCODE
