b847394de1ca2ec39df8beacc2817913117b64df
sylvain beorchia <sylvain.beorchia@oslandia.com>
2016-06-03 12:59:01 +0200

    Fixing migration

--- update/verify_upgrade_db.sh
+++ update/verify_upgrade_db.sh
@@ -101,7 +101,7 @@ do
     CURRENT_DELTA=$(basename "$f")
     #CURRENT_DELTA_NUM_VERSION=$(echo $CURRENT_DELTA| cut -d'_' -f 2)
     CURRENT_DELTA_NUM_VERSION=$(echo $CURRENT_DELTA| cut -c 7)
-    if [[ $CURRENT_DELTA_NUM_VERSION > $SHORT_LATEST_TAG || $SHORT_LATEST_TAG == '' ]]; then
+    if [[ $CURRENT_DELTA_NUM_VERSION >= $SHORT_LATEST_TAG || $SHORT_LATEST_TAG == '' ]]; then
         printf "    Processing ${GREEN}$CURRENT_DELTA${NC}, num version = $CURRENT_DELTA_NUM_VERSION\n"
         /usr/bin/psql --host $HOST --port 5432 --username "$USER" --no-password -q -d "$TESTCONFORMDB" -f $f
     else
@@ -110,6 +110,7 @@ do
 done
 
 echo "Producing referential file for test_qwat DB (from $QWATSERVICETEST)"
+cd $DIR
 /usr/bin/psql --host $HOST --port 5432 --username "$USER" --no-password -d "$QWATSERVICETEST" -f test_migration.sql > test_migration.expected.sql
 
 
