68db2bf8a4340541d45387768815889123820f0f
sylvain beorchia <sylvain.beorchia@oslandia.com>
2016-06-03 13:45:42 +0200

    Cleaning script

--- update/verify_upgrade_db.sh
+++ update/verify_upgrade_db.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 # PARAMS
-SRCDB=qwat
 TESTDB=qwat_test
 TESTCONFORMDB=qwat_test_conform
 USER=postgres
 HOST=localhost
-QWATSERVICE=qwat
 QWATSERVICETEST=qwat_test
 QWATSERVICETESTCONFORM=qwat_test_conform
 
@@ -22,54 +20,27 @@ case $key in
     -h|--help)
         echo "Arguments:"
         echo -e "\t-h|--help\tShow this help screen"
-        echo -e "\t-u|--upgrade\tUpgrade your real DB (perform all deltas on it)"
         exit 0
         ;;
-    -u|--upgrade)
-    UPGRADE_REAL_DB="$2"
-    shift # past argument
-    ;;
 esac
 
 shift
 done
 
-
-        # 1 Create DB test
-        # 2 Launch init_qwat.sh on DB test (QWAT_TEST will be the good DB)
-        # 4 Create DB test_conform
-# 5 Get lastest tagged version of qwat     git describe  => ex: v1.2-2-g91fec13 , extract the latest tag version => NUM_TAG  # TODO if no tag, take the actual repository 
-# 6 Switch on that branch and launch init_qwat.sh on test_conform    git checkout -b version2 v2.0.0
-# 7 switch back on current branch, and launch delta > version tag (NUM_TAG)
-# 8 Generate the results file for DB test and test_conform
-# 9 Compare
-
-# read -s  -p "Please enter the password for you DB user ($USER)": pwd
-# export PGPASSWORD="$pwd"
-# echo ""
-
-# echo "Getting current num version"
-# NUMVERSION=\"$(/usr/bin/psql --host $HOST --port 5432 --username "$USER" --no-password -d "$SRCDB" -c "COPY(SELECT version FROM qwat_sys.versions WHERE module='model.core') TO STDOUT")\"
-# printf "You are currently using qWat v${GREEN}$NUMVERSION${NC}\n"
-
 echo "Droping existing qwat_test"
 /usr/bin/dropdb "$TESTDB" --host $HOST --port 5432 --username "$USER" --no-password
-#/usr/bin/dropdb -d "service=$QWATSERVICETEST"
 
 echo "Creating DB (qwat_test)"
 /usr/bin/createdb "$TESTDB" --host $HOST --port 5432 --username "$USER" --no-password
-#/usr/bin/createdb -d "service=$QWATSERVICETEST"
 
 echo "Initializing qwat DB in qwat_test"
 ./init_qwat.sh -p $QWATSERVICETEST -d > init_qwat.log
 
 echo "Droping DB (qwat_test_conform)"
 /usr/bin/dropdb "$TESTCONFORMDB" --host $HOST --port 5432 --username "$USER" --no-password
-#/usr/bin/createdb -d "service=$QWATSERVICETEST"
 
 echo "Creating DB (qwat_test_conform)"
 /usr/bin/createdb "$TESTCONFORMDB" --host $HOST --port 5432 --username "$USER" --no-password
-#/usr/bin/createdb -d "service=$QWATSERVICETEST"
 
 echo "Getting lastest Tag num"
 cd $DIR
@@ -79,7 +50,7 @@ SHORT_LATEST_TAG=$(echo $LATEST_TAG| cut -c 1)
 printf "    Latest tag = ${GREEN}$SHORT_LATEST_TAG${NC}\n"
 
 
-# !!!!!!!!!    TODO We need to execute init_qwat.sh from the lastest TAG version in $QWATSERVICETESTCONFORM
+# We need to execute init_qwat.sh from the lastest TAG version in $QWATSERVICETESTCONFORM
 # Saving current branch
 echo "Saving current branch"
 #CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
@@ -116,9 +87,7 @@ echo "Producing referential file for test_qwat DB (from $QWATSERVICETEST)"
 cd $DIR
 /usr/bin/psql --host $HOST --port 5432 --username "$USER" --no-password -d "$QWATSERVICETEST" -f test_migration.sql > test_migration.expected.sql
 
-
 #  By the way, this cannot work if there is no TAG version
-
 
 echo "Performing conformity test"
 STATUS=$(python test_migration.py --pg_service $QWATSERVICETESTCONFORM)
@@ -133,11 +102,3 @@ fi
 
 exit $EXITCODE
 
-
-# 
-# echo "Cleaning"
-# rm "$TODAY""_current_qwat.backup"
-# rm init_qwat.log
-# 
-# # TODO : dropping qwat_test
-# # TODO : dropping qwat_test_conform
