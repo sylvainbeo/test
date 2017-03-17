5a8ca811bee7f6249923790b30c699c056e1763e
Sylvain Beorchia <sylvain.beorchia@oslandia.com>
2017-03-17 14:54:13 +0100

    Test travis git clone external repo

--- test_travis.sh
+++ test_travis.sh
@@ -1,3 +1,4 @@
 echo "Git clone from external repo"
 
-git clone https://github.com/qwat/qwat-data-sample.git data-sample
+#git clone https://github.com/qwat/qwat-data-sample.git data-sample
+git clone git@github.com:qwat/qwat-data-sample.git data-sample
